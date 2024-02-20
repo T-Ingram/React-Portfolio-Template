79170014700acb771b041b735a62f3b54ce443e5
Trevor Ingram <ingramtrevor10@gmail.com>
2024-02-19 21:33:51 -0800

    Removed alternate language option

--- src/App.js
+++ src/App.js
@@ -18,35 +18,9 @@ class App extends Component {
     };
   }
 
-  applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
-    document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
-  }
-
-  swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
-  }
-
   componentDidMount = () => {
     this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
+    // You may load resume data here if needed
   }
 
   loadResumeFromPath = (path) => {
@@ -97,7 +71,9 @@ class App extends Component {
             />}>
           </Route>
         </Routes>
-        <Footer/>  
+        <Footer 
+          sharedBasicInfo={this.state.sharedData.basic_info}
+        />  
       </Router>
     );
   }
